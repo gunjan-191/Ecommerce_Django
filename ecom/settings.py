8125a6f782da5a87c49111c44ccd0b4147a04c33
Gunjan Sachdeva <gunjan.sachdeva@lyxelandflamingo.com>
2024-01-26 22:57:14 +0530

    Ecom_Django

--- ecom/settings.py
+++ ecom/settings.py
@@ -144,7 +144,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'gsachdeva705@gmail.com'
-EMAIL_HOST_PASSWORD = 'qnth gkup egoc oecd'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
  
